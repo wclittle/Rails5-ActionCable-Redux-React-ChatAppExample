09a7de139130ef36591369ea89d8196e876688ce
Will Little <wclittle@gmail.com>
2015-12-28 18:01:56 -0600

    Production redis setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://rails5reduxchat.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://rails5reduxchat.herokuapp.com', 'http://rails5reduxchat.herokuapp.com/', 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
